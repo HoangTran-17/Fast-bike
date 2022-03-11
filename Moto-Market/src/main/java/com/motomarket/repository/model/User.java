63c8f05d589504f26fed72f7d2a45ac88c6c343f
DESKTOP-GEFTSHN\ADMIN <tqtteams1st@gmail.com>
2022-03-11 19:43:10 +0700

    Update html edit post and new post

--- Moto-Market/src/main/java/com/motomarket/repository/model/User.java
+++ Moto-Market/src/main/java/com/motomarket/repository/model/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -39,6 +40,7 @@ public class User {
 
     @Column(name = "phone_number")
     private String phoneNumber;
+
 
 
 
